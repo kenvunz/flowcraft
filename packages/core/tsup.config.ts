7fd792a123d252bc1e315a4708ff261e9090b416
Ken Vu <kenvunz@gmail.com>
2026-03-22 23:00:54 +1300

    build(core): add CJS output format

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
 	entry: ['src/**/*.ts'],
 	entryPoints: ['src/index.ts', 'src/testing/index.ts'],
-	format: ['esm'],
+	format: ['esm', 'cjs'],
 	target: 'esnext',
 	dts: true,
 	clean: true,
 	sourcemap: true,
-	splitting: true,
+	splitting: false,
 	treeshake: true,
 	minify: false,
 })
